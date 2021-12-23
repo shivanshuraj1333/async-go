0478f3fe9095464088d420d1df9dfe4db5df71b9
Shivanshu Raj Shrivastava <shivanshu1333@gmail.com>
2021-12-23 23:49:58 +0530

    controlling concurrency

--- Controlling concurrency/main.go
+++ Controlling concurrency/main.go
@@ -1,10 +1,20 @@
 package main
 
+import (
+	"fmt"
+	"time"
+)
+
 var (
 	requestIDs = []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 ,13, 14, 15}
 )
 
 var concurrencyLevel = 3
+
+func makeRequest(ID int) {
+	time.Sleep(time.Second * 2)
+	fmt.Println(ID)
+}
 
 func main() {
 	queue := make(chan bool, concurrencyLevel)
@@ -15,6 +25,7 @@ func main() {
 			defer func() {
 				<- queue
 			}()
-		}
+			makeRequest(ID)
+		}(_ID)
 	}
 }
