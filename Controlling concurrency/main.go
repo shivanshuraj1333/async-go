e653992151a8edc41439f82aed3d1f4c4b842f32
Shivanshu Raj Shrivastava <shivanshu1333@gmail.com>
2021-12-23 23:47:38 +0530

    initial commit2

--- Controlling concurrency/main.go
+++ Controlling concurrency/main.go
@@ -1,5 +1,20 @@
 package main
 
-func main() {
+var (
+	requestIDs = []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 ,13, 14, 15}
+)
 
+var concurrencyLevel = 3
+
+func main() {
+	queue := make(chan bool, concurrencyLevel)
+
+	for _, _ID := range requestIDs {
+		queue <- true
+		go func (ID int) {
+			defer func() {
+				<- queue
+			}()
+		}
+	}
 }
